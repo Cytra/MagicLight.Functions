b3fda1da68faead11736693b212d5b25f6b38be5
Cytra <samuilovas@gmail.com>
2019-10-01 23:16:42 +0300

    second commit

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,22 @@
 import * as functions from 'firebase-functions';
+// import * as admin from 'firebase-admin';
+import * as sgMail from '@sendgrid/mail';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const API_KEY = functions.config().sendgrid.key;
+const TEMPLATE_ID = functions.config().sendgrid.template;
+sgMail.setApiKey(API_KEY);
+
+export const generateMail = functions.https.onCall(async (data, context) => {
+    const msg = {
+        to: 'info@magiclight.lt',
+        from: 'info@magiclight.lt',
+        templateId: TEMPLATE_ID,
+        dynamic_template_data: {
+            subject: "subject",
+            name: "name",
+        },
+    };
+
+    await sgMail.send(msg);
+})
+
