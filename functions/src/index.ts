69bf446e5fca0b494e84e3230144ef717c9ad464
Cytra <samuilovas@gmail.com>
2019-10-02 21:48:01 +0300

    added contactUsa and newOrder email functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -18,5 +18,48 @@ export const generateMail = functions.https.onCall(async (data, context) => {
     };
 
     await sgMail.send(msg);
-})
+});
+
+export const contactUs = functions.https.onCall(async (data, context) => {
+    const msg = {
+        to: 'info@magiclight.lt',
+        from: 'info@magiclight.lt',
+        templateId: 'd-8f6f1f41238742c799925a8fdb6b3fde',
+        dynamic_template_data: {
+            name: data.name,
+            email: data.email,
+            subject: data.subject,
+            content: data.content,
+        },
+    };
+    await sgMail.send(msg);
+
+    return {success: true};
+});
+
+export const neworder = functions.https.onCall(async (data, context) => {
+    const msg = {
+        to: 'info@magiclight.lt',
+        from: 'info@magiclight.lt',
+        templateId: 'd-a9d389047ee6420cb64eaeb2b481c5b7',
+        dynamic_template_data: {
+            name: data.name,
+            lastName: data.lastName,
+            address: data.address,
+            city: data.city,
+            country: data.country,
+            zipCode: data.zipCode,
+            email: data.email,
+            phone: data.phone,
+            addInfo: data.addInfo,
+            products: data.products,
+            price: data.price,
+            shipping: data.shipping,
+            willpay: data.willpay,
+        },
+    };
+
+    await sgMail.send(msg);
+    return {success: true};
+});
 
